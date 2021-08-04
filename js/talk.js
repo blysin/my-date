5309e2097d61a9b4b068f6ce32418f2e48b85ffb
戴少坤 <daishaokun@xiaoyouzi.com>
2021-08-04 17:54:16 +0800

    init

---
+++ js/talk.js
@@ -0,0 +1,18 @@
+﻿new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("晚上一起吃饭ヽ(^ω^)ﾉ  ")
+    .pause(5000)
+    .delete(null, {
+        delay: 500
+    })
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
